a4b28735e08ee29b15339fd39b8f568c552fc13d
Nathan Allan <nate@digithought.com>
2026-03-24 10:31:05 -0600

    Fixed pub package

--- scripts/publish-package.js
+++ scripts/publish-package.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
-const { execSync } = require('child_process');
-const { join } = require('path');
+import { execSync } from 'child_process';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const rootDir = join(__dirname, '..');
 const packageName = process.argv[2];
 
